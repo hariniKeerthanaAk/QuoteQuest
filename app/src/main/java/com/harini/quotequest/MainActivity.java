7ee23b820ea424370bc821a87998b426e4d445d4
hariniKeerthanaAk <harinikeerthana.a2022eee@sece.ac.in>
2024-01-25 08:33:03 +0530

    Added favorite button

--- app/src/main/java/com/harini/quotequest/MainActivity.java
+++ app/src/main/java/com/harini/quotequest/MainActivity.java
@@ -1,14 +1,32 @@
 package com.harini.quotequest;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private ImageButton favButtonBorder;
+    private ImageButton favButtonFull;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        favButtonBorder = findViewById(R.id.favButtonBorder);
+        favButtonFull = findViewById(R.id.favButtonFull);
+
+        favButtonBorder.setOnClickListener(v -> {
+            favButtonBorder.setVisibility(View.GONE);
+            favButtonFull.setVisibility(View.VISIBLE);
+        });
+
+        favButtonFull.setOnClickListener(v -> {
+            favButtonFull.setVisibility(View.GONE);
+            favButtonBorder.setVisibility(View.VISIBLE);
+        });
     }
 }
